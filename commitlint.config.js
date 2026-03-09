7a4f8c5280abe24af818f5c24ceea78f500cd2f9
isauranina <ns.isaura@gmail.com>
2026-03-08 21:58:54 -0400

    ci: configuracion husky para commit-msg

---
+++ commitlint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
+ //Usa:
+ // chore para tares,
+ // feat:agregar endpoint,
+ // fix:Corregir errores,
+ // refactor:Refactorizar codigo,
+ // test:Agregar tests,
+ // docs:Agregar documentación,
+ // style:Cambiar estilo de codigo,
+ // perf:Mejorar rendimiento,
+ // chore:Tareas de mantenimiento,
+ // ci:Cambios en la configuración de CI,
+ // build:Cambios en la configuración de build,
+ // revert:Revertir cambios,
+ // merge:Fusionar cambios,
+ // release:Lanzamiento de nueva versión"
