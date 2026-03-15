2bdb1a384e18645bd5ca01387a216b68d76b0d2d
isauranina <ns.isaura@gmail.com>
2026-03-15 18:21:48 -0400

    chore: se añade format-cs configuracion para configuracion de  lint-staget

--- src/Transacciones.Core/DTOs/Transacciones/CrearAbonoDto.cs
+++ src/Transacciones.Core/DTOs/Transacciones/CrearAbonoDto.cs
@@ -1,7 +1,6 @@
 namespace Transacciones.Core.DTOs.Transacciones;
 
-public class CrearAbonoDto
-	{
+public class CrearAbonoDto {
 	public int CuentaId { get; set; }
 	public decimal Monto { get; set; }
 	public string Descripcion { get; set; } = string.Empty;
