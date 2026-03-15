16d5fbfe13e042471948890feb3df76cd10cc680
isauranina <ns.isaura@gmail.com>
2026-03-15 17:15:46 -0400

    style: alteracion de formato para probar lint-staged

--- src/Transacciones.Core/DTOs/Transacciones/CrearAbonoDto.cs
+++ src/Transacciones.Core/DTOs/Transacciones/CrearAbonoDto.cs
@@ -1,6 +1,7 @@
 namespace Transacciones.Core.DTOs.Transacciones;
 
-public class CrearAbonoDto {
+public class CrearAbonoDto
+	{
 	public int CuentaId { get; set; }
 	public decimal Monto { get; set; }
 	public string Descripcion { get; set; } = string.Empty;
