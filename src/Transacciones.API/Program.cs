71eacf0d01c3a4596c194d35a9999fe9b10244d1
isauranina <ns.isaura@gmail.com>
2026-03-31 03:14:10 -0400

    chore: ajuste program para ver swagger

--- src/Transacciones.API/Program.cs
+++ src/Transacciones.API/Program.cs
@@ -129,10 +129,14 @@ app.UseCors("CorsPolicy");
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment()) {
+app.UseSwagger();
+//app.UseSwaggerUI();
+app.UseSwaggerUI(options => {
+	options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+	options.RoutePrefix = string.Empty; // Esto permite ver Swagger en http://tu-ip/
+});
+//}
 
 //pp.UseHttpsRedirection();
 app.UseAuthentication();
